d2a4308ff9825672953a02d8079b41edeced0a97
chafidz05 <chafidz0505@gmail.com>
2025-02-13 14:56:42 +0700

    V1.0 Setup

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
